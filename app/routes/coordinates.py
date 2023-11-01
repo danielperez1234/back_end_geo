12fbe742cdfafbd5780203e59e948193706933c5
Josue Meza <jml74262@lasallebajio.edu.mx>
2023-10-31 19:26:01 -0600

    Comentarios -Se agregan comentarios al código y bloques de try catch

--- app/routes/coordinates.py
+++ app/routes/coordinates.py
@@ -4,52 +4,93 @@ from app import db
 
 coordinate_bp = Blueprint('coordinate', __name__)
 
+#Métodos para la API de coordenadas
+#Se crea una nueva coordenada
 @coordinate_bp.route('/', methods=['POST'])
 def create_coordinate():
-    data = request.get_json()
-    latitude = data['latitude']
-    longitude = data['longitude']
+    try:
+      #Se obtienen los datos de la petición
+      data = request.get_json()
+      latitude = data['latitude']
+      longitude = data['longitude']
 
-    new_coordinate = Coordinates(latitude=latitude, longitude=longitude)
-    db.session.add(new_coordinate)
-    db.session.commit()
+      #Se crea la nueva coordenada
+      new_coordinate = Coordinates(latitude=latitude, longitude=longitude)
+      #Se agrega la nueva coordenada a la base de datos
+      db.session.add(new_coordinate)
+      #Se confirman los cambios
+      db.session.commit()
+      #Se retorna un mensaje de éxito
+      return jsonify({'message': 'Coordenada creada exitosamente'})
+    except:
+      #Si ocurre un error, se retorna un mensaje de error 400
+      return jsonify({'message': 'Error al crear la coordenada'}), 400
 
-    return jsonify({'message': 'Coordenada creada exitosamente'})
-
+#Se obtienen todas las coordenadas
 @coordinate_bp.route('/', methods=['GET'])
 def get_coordinates():
-    coordinates = Coordinates.query.all()
-    return jsonify([{'latitude': coord.latitude, 'longitude': coord.longitude} for coord in coordinates])
+    try:
+      #Se obtienen todas las coordenadas de la base de datos
+      coordinates = Coordinates.query.all()
+      #Se retornan las coordenadas en formato JSON
+      return jsonify([{'latitude': coord.latitude, 'longitude': coord.longitude} for coord in coordinates])
+    except:
+      #Si ocurre un error, se retorna un mensaje de error 404
+      return jsonify({'message': 'Coordenadas no encontradas'}), 404
 
+#Se obtiene una coordenada por su id
 @coordinate_bp.route('/<int:id>', methods=['GET'])
 def get_coordinate(id):
-    coordinate = Coordinates.query.get(id)
-    if coordinate:
-        return jsonify({'latitude': coordinate.latitude, 'longitude': coordinate.longitude})
-    else:
-        return jsonify({'message': 'Coordenada no encontrada'}), 404
+    try:
+      #Se obtiene la coordenada de la base de datos por su id
+      coordinate = Coordinates.query.get(id)
+      #Si la coordenada existe, se retorna en formato JSON
+      if coordinate:
+          return jsonify({'latitude': coordinate.latitude, 'longitude': coordinate.longitude})
+      #Si no existe, se retorna un mensaje de error 404
+      else:
+          return jsonify({'message': 'Coordenada no encontrada'}), 404
+    except:
+      #Si ocurre un error, se retorna un mensaje de error 404
+      return jsonify({'message': 'Coordenada no encontrada'}), 404
 
+#Se actualiza una coordenada por su id
 @coordinate_bp.route('/<int:id>', methods=['PUT'])
 def update_coordinate(id):
-    coordinate = Coordinates.query.get(id)
+    try:
+      #se obtiene la coordenada de la base de datos por su id
+      coordinate = Coordinates.query.get(id)
+      #Si la coordenada existe, se actualiza
+      if coordinate:
+          #Se obtienen los datos de la petición
+          data = request.get_json()
+          coordinate.latitude = data.get('latitude', coordinate.latitude)
+          coordinate.longitude = data.get('longitude', coordinate.longitude)
+          #Se confirman los cambios
+          db.session.commit()
+          #Se retorna un mensaje de éxito
+          return jsonify({'message': 'Coordenada actualizada exitosamente'})
+      #Si no existe, se retorna un mensaje de error 404
+      else:
+          return jsonify({'message': 'Coordenada no encontrada'}), 404
+    except:
+      #Si ocurre un error, se retorna un mensaje de error 400
+      return jsonify({'message': 'Error al actualizar la coordenada'}), 400
 
-    if coordinate:
-        data = request.get_json()
-        coordinate.latitude = data.get('latitude', coordinate.latitude)
-        coordinate.longitude = data.get('longitude', coordinate.longitude)
-
-        db.session.commit()
-        return jsonify({'message': 'Coordenada actualizada exitosamente'})
-    else:
-        return jsonify({'message': 'Coordenada no encontrada'}), 404
-
+#Se elimina una coordenada por su id
 @coordinate_bp.route('/<int:id>', methods=['DELETE'])
 def delete_coordinate(id):
-    coordinate = Coordinates.query.get(id)
-
-    if coordinate:
-        db.session.delete(coordinate)
-        db.session.commit()
-        return jsonify({'message': 'Coordenada eliminada exitosamente'})
-    else:
-        return jsonify({'message': 'Coordenada no encontrada'}), 404
+    try:
+      #Se obtiene la coordenada de la base de datos por su id
+      coordinate = Coordinates.query.get(id)
+      #Si la coordenada existe, se elimina
+      if coordinate:
+          db.session.delete(coordinate)
+          db.session.commit()
+          return jsonify({'message': 'Coordenada eliminada exitosamente'})
+      #Si no existe, se retorna un mensaje de error 404
+      else:
+          return jsonify({'message': 'Coordenada no encontrada'}), 404
+    except:
+      #Si ocurre un error, se retorna un mensaje de error 400
+      return jsonify({'message': 'Error al eliminar la coordenada'}), 400
--- app/routes/style.py
+++ app/routes/style.py
@@ -4,50 +4,70 @@ from app import db
 
 style_bp = Blueprint('style', __name__)
 
+# Método para crear un estilo
 @style_bp.route('/', methods=['POST'])
 def create_style():
-    data = request.get_json()
-    name = data['name']
+    try:
+        data = request.get_json()
+        name = data['name']
 
-    new_style = Style(name=name)
-    db.session.add(new_style)
-    db.session.commit()
+        new_style = Style(name=name)
+        db.session.add(new_style)
+        db.session.commit()
 
-    return jsonify({'message': 'Estilo creado exitosamente'})
+        return jsonify({'message': 'Estilo creado exitosamente'})
+    except:
+        return jsonify({'message': 'Error al crear el estilo'}), 400
 
+# Método para obtener todos los estilos
 @style_bp.route('/', methods=['GET'])
 def get_styles():
-    styles = Style.query.all()
-    return jsonify([{'id': style.id, 'name': style.name} for style in styles])
+    try:
+        styles = Style.query.all()
+        return jsonify([{'id': style.id, 'name': style.name} for style in styles])
+    except:
+        return jsonify({'message': 'Estilos no encontrados'}), 404
 
+# Método para obtener un estilo por su id
 @style_bp.route('/<int:id>', methods=['GET'])
 def get_style(id):
-    style = Style.query.get(id)
-    if style:
-        return jsonify({'id': style.id, 'name': style.name})
-    else:
-        return jsonify({'message': 'Estilo no encontrado'}), 404
+    try:
+        style = Style.query.get(id)
+        if style:
+            return jsonify({'id': style.id, 'name': style.name})
+        else:
+            return jsonify({'message': 'Estilo no encontrado'}), 404
+    except:
+        return jsonify({'message': 'Error al obtener el estilo'}), 500
 
+# Método para actualizar un estilo por su id
 @style_bp.route('/<int:id>', methods=['PUT'])
 def update_style(id):
-    style = Style.query.get(id)
+    try:
+        style = Style.query.get(id)
 
-    if style:
-        data = request.get_json()
-        style.name = data.get('name', style.name)
+        if style:
+            data = request.get_json()
+            style.name = data.get('name', style.name)
 
-        db.session.commit()
-        return jsonify({'message': 'Estilo actualizado exitosamente'})
-    else:
-        return jsonify({'message': 'Estilo no encontrado'}), 404
+            db.session.commit()
+            return jsonify({'message': 'Estilo actualizado exitosamente'})
+        else:
+            return jsonify({'message': 'Estilo no encontrado'}), 404
+    except:
+        return jsonify({'message': 'Error al actualizar el estilo'}), 500
 
+# Método para eliminar un estilo por su id
 @style_bp.route('/<int:id>', methods=['DELETE'])
 def delete_style(id):
-    style = Style.query.get(id)
+    try:
+        style = Style.query.get(id)
 
-    if style:
-        db.session.delete(style)
-        db.session.commit()
-        return jsonify({'message': 'Estilo eliminado exitosamente'})
-    else:
-        return jsonify({'message': 'Estilo no encontrado'}), 404
+        if style:
+            db.session.delete(style)
+            db.session.commit()
+            return jsonify({'message': 'Estilo eliminado exitosamente'})
+        else:
+            return jsonify({'message': 'Estilo no encontrado'}), 404
+    except:
+        return jsonify({'message': 'Error al eliminar el estilo'}), 500
